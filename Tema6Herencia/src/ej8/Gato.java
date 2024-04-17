d4f11dbcb05d3593b7b4e66b64ff1c1fefbf0da2
Marco55H <145675712+Marco55H@users.noreply.github.com>
2024-04-17 13:12:00 +0200

    Antes de feria

--- Tema6Herencia/src/ej8/Gato.java
+++ Tema6Herencia/src/ej8/Gato.java
@@ -1,5 +1,36 @@
 package ej8;
 
-public class Gato {
+import java.util.Random;
 
+public class Gato extends AnimalDoméstico{
+
+	static Random r = new Random();
+	
+	public Gato(String nombre, String raza, String color, double peso) {
+		super(nombre, raza, color, peso);
+	}
+
+	@Override
+	public boolean hacerCaso() {
+		int porcentaje;
+		boolean caso=false;
+		
+		porcentaje=r.nextInt(0,101);
+		
+		if (porcentaje<6) {
+			caso=true;
+		}
+		return caso;
+	}
+
+	@Override
+	public void hacerRuido() {
+		System.out.println("MIAU");
+		
+	}
+
+	public void toserBolaPelo(){
+		System.out.println("Ha 	tosido una bola de pelo");
+	}
+	
 }
--- Tema6Herencia/src/ej8/Perro.java
+++ Tema6Herencia/src/ej8/Perro.java
@@ -1,5 +1,37 @@
 package ej8;
 
-public class Perro {
+import java.util.Random;
 
+public class Perro extends AnimalDoméstico{
+
+	public Perro(String nombre, String raza, String color, double peso) {
+		super(nombre, raza, color, peso);
+	}
+
+	static Random r = new Random();
+
+	@Override
+	public boolean hacerCaso() {
+		int porcentaje;
+		boolean caso=false;
+		
+		porcentaje=r.nextInt(0,101);
+		
+		if (porcentaje<91) {
+			caso=true;
+		}
+		return caso;
+	}
+
+	@Override
+	public void hacerRuido() {
+		System.out.println("GUAU");
+		
+	}
+
+	public void sacarPaseo(){
+		System.out.println("Paseado");
+	}
+	
+	
 }
